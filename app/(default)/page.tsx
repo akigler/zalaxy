9f4c3333f7583f928c100e36f5b3b8cc77e94277
Aaron Kigler <aaronkigler@gmail.com>
2023-09-07 13:17:46 -0400

    title

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -8,10 +8,12 @@ import Features from "@/components/features";
 import FeaturesBlocks from "@/components/features-blocks";
 import Testimonials from "@/components/testimonials";
 import Newsletter from "@/components/newsletter";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>{metadata.title}</Head>
       <Hero />
       <FeaturesBlocks />
     </>
